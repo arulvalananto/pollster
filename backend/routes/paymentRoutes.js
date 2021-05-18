b2da96e33dae9170ccfdccfa4955aac29480d2f5
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2021-05-19 00:15:59 +0530

    update #1

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -22,3 +22,40 @@ router.post("/api/stripe", isLogin, async (req, res) => {
 });
 
 module.exports = router;
+
+// Alternative way of payment (this is not included in this project)
+// Creating Session
+
+//******************* Server Side *****************/
+
+// api/v1/checkoutsession/:tourId
+
+// exports.getCheckoutSession = catchAsync(async (req, res) => {
+//     const tour = await Tour.findById(req.params.tourId);
+
+//     const session = await stripe.checkout.sessions.create({
+//         payment_method_types: ["card"],
+//         success_url: `${req.protocol}://${req.get("host")}/`,
+//         cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
+//         customer_email: req.user.email,
+//         client_reference_id: req.params.tourId,
+//         line_items: [
+//             {
+//                 name: `${tour.name} Tour`,
+//                 description: tour.summary,
+//                 images: ["image_url.png"],
+//                 amount: tour.price * 100,
+//                 currency: "usd",
+//                 quantity: 1,
+//             },
+//         ],
+//     });
+//     res.status(200).json({
+//         message: "success",
+//         session,
+//     });
+// });
+
+// ********************* Client Side ***************/
+
+//  For More Informtion, check stripe payment documentation
